6cf1a92fbce25bfcc5550096b9f69bcc49e9e579
Nicolas Agudelo <nicolas.agudelog@autonoma.edu.co>
2026-02-02 22:30:39 -0500

    All done , class team added bolean alive to check if the team is alive or no

--- src/main/scala/BattleField.scala
+++ src/main/scala/BattleField.scala
@@ -29,39 +29,85 @@ object BattleField extends App {
   var allNumbers = ArrayBuffer[Number]()
 
 
-  allNumbers+= new Number(8,1,2,0, true)
-  allNumbers+= new Number(5,2,0,1, false )
-  allNumbers+= new Number(4,4,2,2,false)
-  allNumbers+= new Number(6,1,2,3,true)
-  allNumbers+= new Number(8,3,0,4,false)
-  allNumbers+= new Number(5,5,0,5,false)
-  allNumbers+= new Number(3,2,5,6,true)
-  allNumbers+= new Number(4,3,0,7,false)
-  allNumbers+= new Number(2,2,6,8,true)
-  allNumbers+= new Number(5,6,1,9, false)
+  allNumbers+= new Number(16,1,2,0, true)
+  allNumbers+= new Number(10,2,0,1, false )
+  allNumbers+= new Number(8,4,2,2,false)
+  allNumbers+= new Number(12,1,2,3,true)
+  allNumbers+= new Number(16,3,0,4,false)
+  allNumbers+= new Number(10,5,0,5,false)
+  allNumbers+= new Number(6,2,5,6,true)
+  allNumbers+= new Number(8,3,0,7,false)
+  allNumbers+= new Number(4,2,6,8,true)
+  allNumbers+= new Number(10,6,1,9, false)
 
   class Team(numberBuffer: ArrayBuffer[Int]) {
     val teamName : String = numberBuffer.mkString
-    protected var teamHp: Int = 0
+    var teamHp: Int = 0 // volver protected despues
+    protected var alive : Boolean = true //boleano que se verifica cada ronda para ver si algun equipo sigue vivo
+    protected  var maxTeamHp : Int=0
     protected var teamAtk : Int = 0
     protected var teamMp : Int = 0
-    protected var tHabilities : List[Hability]
+    protected var tHabilities = ArrayBuffer[Hability]()
+
+    setTeam()
 
     def setTeam(): Unit={
-      var TNumbers = ArrayBuffer[Number]()
-      for(number<- numberBuffer)
-        for(i <- allNumbers)
+
+      for(number<- numberBuffer) {
+
+        for(i <- allNumbers) {
+
           if(i.id == number){
-            TNumbers += i
-
+            teamHp += i.hp
+            teamAtk += i.atk
+            teamMp += i.mp
+            tHabilities += i.hability
           }
+        }
+      }
+      maxTeamHp = teamHp
+    }
 
+    def modifyHp(modiffier: Int): Unit={
+      teamHp += modiffier
+      if(teamHp<=0) alive=false
+    }
+
+    def useAction(enemyTeam : Team ):Unit = {
+      val rand = new scala.util.Random
+      val randNumb = rand.nextInt(tHabilities.length)
+      val habSelected = tHabilities(randNumb)
+      var habValue: Int = 0
+      if (habSelected.heals == true){
+        habValue = habSelected.useHability(teamAtk, teamMp)
+        if((habValue+teamHp)>= maxTeamHp){
+          teamHp = maxTeamHp
+        }else{
+          teamHp += habValue
+        }
+
+      } else {
+        habValue = -1 * (habSelected.useHability(teamAtk, teamMp)) // valor negativo pq hace daño
+        enemyTeam.modifyHp(habValue)
+      }
     }
 
 
   }
 
+  //codigo para comprovar creación de equipos y testeo de daño
 
+  /*var list1 = new ArrayBuffer[Int]()
+  list1 += 1
+  list1 += 2
+  list1 += 3
+  val list2 = list1.map(x=>x+1)
+  var team1: Team = new Team(list1)
+  var team2: Team = new Team(list2)
+  println("vida " + team1.teamHp)
+  team2.useAction(team1)
+  println("vida " + team1.teamHp)
+*/
 
 
 }
